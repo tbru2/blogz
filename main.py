d46464c20a09b003579f610068063d8e249f3292
tbru2 <tbru2@pdx.edu>
2017-06-28 16:01:21 -0700

    started

---
+++ main.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, redirect, request
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+app.config['SQLALCHEMY_DATABASE_URI']= 'mysql+pymysql://build-a-blog:12345@localhost:8889/build-a-blog'
+app.config['SQLALCHEMY_ECHO'] = True
+db = SQLAlchemy(app)
+
+class Blog(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(120))
+    body = db.Column(db.String(1000))
+
+    def __init__(self, title):
+        self.title = title
+
+@app.route('/blog', methods=['GET'])
+def index():
+    if request.method == 
+
+@app.route('/newpost', methods=['POST'])
